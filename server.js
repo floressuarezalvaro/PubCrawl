144fb17510f4adcad05140be75c57c6e43eecfb8
Nicolas Martinez <Nimarti548@gmail.com>
2021-03-09 20:55:17 -0800

    UPDATE: added auth code

--- server.js
+++ server.js
@@ -14,13 +14,14 @@ if (process.env.NODE_ENV === "production") {
 // this is where I would put them
 // app.use("/users", require("./routes/userRoutes"));
 app.use("/bars", require("./routes/apiRoutes"));
+app.use("/users", require("./routes/userRoutes"))
 
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
 mongoose.connect(
-  process.env.MONGO_URI || "mongodb://localhost/PubCrawl",
+  process.env.MONGO_URI || ("mongodb://localhost/PubCrawl"),
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
