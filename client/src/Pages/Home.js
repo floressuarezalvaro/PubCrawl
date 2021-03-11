39dea952155c83ed446ad5093bbc0c71d4377f69
“bonnielee” <bonlee8489@gmail.com>
2021-03-10 18:50:34 -0800

    edits to home.js and search.js and components

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import {useHistory} from "react-router-dom";
 
 const Home = () => {
+  const { userData } = useContext(UserContext);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (!userData.user) history.push("/search");
+  }, [userData.user, history]);
+
   return (
-    <div>
-      <h1>Hello from Home Page</h1>
+    <div className="container">
+      <h1>Home Page</h1>
+      <p>Let's go Bar Hopping!</p>
+      <button onClick = {() => history.push("/search")} className="btn btn-outline-primary">Submit</button>
     </div>
   );
 };
