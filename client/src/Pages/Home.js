70721b82c356a9b82d1a2cd98aa474b5d7785e95
“bonnielee” <bonlee8489@gmail.com>
2021-03-10 19:33:08 -0800

    updates to Components folder and home.js

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,19 +1,19 @@
 import React, { useContext, useEffect } from "react";
-import {useHistory} from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import UserContext from "../Context/UserContext"
 
-const Home = () => {
-  const { userData } = useContext(UserContext);
-  const history = useHistory();
+const Home = (props) => {
+const { userData } = useContext(UserContext);
+const history = useHistory();
 
-  useEffect(() => {
-    if (!userData.user) history.push("/search");
-  }, [userData.user, history]);
-
+useEffect(() => {
+if(!userData.user) history.push("/login");
+}, [userData.user, history])
   return (
-    <div className="container">
-      <h1>Home Page</h1>
-      <p>Let's go Bar Hopping!</p>
-      <button onClick = {() => history.push("/search")} className="btn btn-outline-primary">Submit</button>
+    <div>
+      <h1>Hello from Home Page</h1>
+      <h3>Hello {userData.user?.displayName}</h3>
+      <button onClick={() => history.push("/search")} className="btn btn-outline-primary">Submit</button>
     </div>
   );
 };
