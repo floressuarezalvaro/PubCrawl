82cb9a5e56064d469c850a82d77184f7d45ee61a
bracamon <bracamontes.ochoa@gmail.com>
2021-03-11 21:50:39 -0700

    search and card components

--- client/src/Components/Cards.js
+++ client/src/Components/Cards.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css";
 
 const Cards = () => {
-  return <div></div>;
+  // useEffect(() => {
+  //   var elems = document.querySelectorAll(".sidenav");
+  //   var instances = M.Sidenav.init(elems, {});
+  // }, []);
+
+  return (
+    <div>
+      <div className="row">
+        <div className="col s12 m6">
+          <div className="card">
+            <div className="card-image">
+              {/* <img src="images/sample-1.jpg" /> */}
+              <span className="card-title">Card Title</span>
+              <a className="btn-floating halfway-fab waves-effect waves-light red">
+                <i className="material-icons">add</i>
+              </a>
+            </div>
+            <div className="card-content">
+              <p>
+                I am a very simple card. I am good at containing small bits of
+                information. I am convenient because I require little markup to
+                use effectively.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Cards;
--- client/src/Pages/Search.js
+++ client/src/Pages/Search.js
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
+import Cards from "../Components/Cards";
+import Navbar from "../Components/Navbar";
+import SearchBar from "../Components/SearchBar";
 
 const Search = () => {
   const [] = useState([]);
   return (
     <div>
+      <Navbar />
+      <SearchBar />
+      <Cards />
       <h1>Hello from Search Page</h1>
     </div>
   );
