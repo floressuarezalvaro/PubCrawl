40e8141cef5a7a794e2ea9225e4ae00eeecf46aa
bracamon <bracamontes.ochoa@gmail.com>
2021-03-11 20:07:01 -0700

    install materialize and set up navbar component

--- client/src/Components/Nav.js
+++ client/src/Components/Nav.js
@@ -1,11 +1,105 @@
-import React from 'react'
+import React, { Component } from "react";
 
-const Nav = () => {
+class Nav extends Component {
+  componentDidMount() {
+    const M = window.M;
+    document.addEventListener("DOMContentLoaded", function () {
+      var elems = document.querySelectorAll(".sidenav");
+      var instances = M.Sidenav.init(elems, {});
+    });
+  }
+
+  render() {
     return (
-        <div>
-            
-        </div>
-    )
+      <div>
+        <nav>
+          <div className="nav-wrapper">
+            <a href="#!" className="brand-logo">
+              Logo
+            </a>
+            <a href="#" data-target="mobile-demo" className="sidenav-trigger">
+              <i className="material-icons">menu</i>
+            </a>
+            <ul className="right hide-on-med-and-down">
+              <li>
+                <a href="sass.html">Sass</a>
+              </li>
+              <li>
+                <a href="badges.html">Components</a>
+              </li>
+              <li>
+                <a href="collapsible.html">Javascript</a>
+              </li>
+              <li>
+                <a href="mobile.html">Mobile</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        <ul className="sidenav" id="mobile-demo">
+          <li>
+            <a href="sass.html">Sass</a>
+          </li>
+          <li>
+            <a href="badges.html">Components</a>
+          </li>
+          <li>
+            <a href="collapsible.html">Javascript</a>
+          </li>
+          <li>
+            <a href="mobile.html">Mobile</a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
+
+// const Nav = () => {
+//   return (
+//     <div>
+//       <nav>
+//         <div className="nav-wrapper">
+//           <a href="#!" className="brand-logo">
+//             Logo
+//           </a>
+//           <a href="#" data-target="mobile-demo" className="sidenav-trigger">
+//             <i className="material-icons">menu</i>
+//           </a>
+//           <ul className="right hide-on-med-and-down">
+//             <li>
+//               <a href="sass.html">Sass</a>
+//             </li>
+//             <li>
+//               <a href="badges.html">Components</a>
+//             </li>
+//             <li>
+//               <a href="collapsible.html">Javascript</a>
+//             </li>
+//             <li>
+//               <a href="mobile.html">Mobile</a>
+//             </li>
+//           </ul>
+//         </div>
+//       </nav>
+
+//       <ul className="sidenav" id="mobile-demo">
+//         <li>
+//           <a href="sass.html">Sass</a>
+//         </li>
+//         <li>
+//           <a href="badges.html">Components</a>
+//         </li>
+//         <li>
+//           <a href="collapsible.html">Javascript</a>
+//         </li>
+//         <li>
+//           <a href="mobile.html">Mobile</a>
+//         </li>
+//       </ul>
+//     </div>
+//   );
+// };
 
 export default Nav;
