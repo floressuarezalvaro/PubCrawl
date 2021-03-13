ba3662f99a33316d005774f31e42ca68dba3ccae
bracamon <bracamontes.ochoa@gmail.com>
2021-03-13 13:04:53 -0700

    display user name in card on profile page

--- client/src/Components/Cards.js
+++ client/src/Components/Cards.js
@@ -10,27 +10,29 @@ const Cards = () => {
 
   return (
     <div>
-      <div className="row" id="barList">
-        <div className="col s12 m6 l4">
-          <div className="card grey lighten-5">
-            <div className="card-content grey-text">
-              <p className="#bar-name">bar name</p>
-              <p className="#city-name">city name</p>
-              <p className="#state">state</p>
-              <div className="card-action"></div>
-              <a className="btn-floating halfway waves-effect waves-light red">
-                <i className="material-icons">favorite</i>
-              </a>
-              <a className="btn-floating waves-effect waves-light red">
-                <i className="material-icons">add</i>
-              </a>
-              <a
-                className="waves-effect waves-light btn modal-trigger amber-text grey darken-3"
-                data-id="0"
-                href="#modal1"
-              >
-                button
-              </a>
+      <div className="container">
+        <div className="row" id="barList">
+          <div className="col s12 m6 l6">
+            <div className="card grey lighten-5">
+              <div className="card-content grey-text">
+                <p className="#bar-name">bar name</p>
+                <p className="#city-name">city name</p>
+                <p className="#state">state</p>
+                <div className="card-action"></div>
+                <a className="btn-floating halfway waves-effect waves-light red">
+                  <i className="material-icons">favorite</i>
+                </a>
+                <a className="btn-floating waves-effect waves-light red">
+                  <i className="material-icons">add</i>
+                </a>
+                <a
+                  className="waves-effect waves-light btn modal-trigger amber-text grey darken-3"
+                  data-id="0"
+                  href="#modal1"
+                >
+                  button
+                </a>
+              </div>
             </div>
           </div>
         </div>
